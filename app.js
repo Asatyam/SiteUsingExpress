57281a5e83cd1c213494c1cd3281f7420caf8a44
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2022-10-05 23:45:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,22 +5,22 @@ const port = 3000;
 
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html', { root: __dirname })
+    res.sendFile('./index.html', { root: __dirname })
     console.log(req.url);
 })
 
 
- app.get('/about', (req, res) => {
-    res.sendFile('about.html', { root: __dirname });
+app.get('/about', (req, res) => {
+    res.sendFile('./about.html', { root: __dirname });
 })
 app.get('/contact-me', (req, res) => {
-    res.sendFile('contact-me.html', { root: __dirname });
-}) 
-
-app.get('*',(req,res)=>{
-    res.status(404).sendFile('404.html',{root:__dirname});
+    res.sendFile('./contact-me.html', { root: __dirname });
 })
 
-app.listen(port,()=>{
+app.get('*', (req, res) => {
+    res.status(404).sendFile('./404.html', { root: __dirname });
+})
+
+app.listen(port, () => {
     console.log(`Example server running at ${port}`)
 })

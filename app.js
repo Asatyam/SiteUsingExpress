6a6181d40893e4935c30bd10e3d0f30410aee8db
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2022-10-06 00:28:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
 const express = require('express');
-const fs = require('fs').promises;
 const app = express();
 const port = 3000;
 const path = require('path')
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'))
-    console.log(__dirname);
+    console.log(path.join(__dirname, '/index.html'));
 })
-
-
 app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, '/about.html'));
 })
 app.get('/contact-me', (req, res) => {
     res.sendFile(path.join(__dirname,'/contact-me.html'));
 })
-
 app.get('*', (req, res) => {
     res.status(404).sendFile(path.join(__dirname, '/404.html'));
 })
-
 app.listen(port, () => {
     console.log(`Example server running at ${port}`)
 })

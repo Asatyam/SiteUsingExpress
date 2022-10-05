94a89f48e6b4d43ecb00f71f039f8d22b062e576
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2022-10-05 23:59:33 +0530

    added path package

--- app.js
+++ app.js
@@ -5,20 +5,20 @@ const port = 3000;
 const path = require('path')
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.html'))
-    console.log(req.url);
+    res.sendFile(path.join(__dirname, '/index.html'))
+    console.log(__dirname);
 })
 
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, './about.html'));
+    res.sendFile(path.join(__dirname, '/about.html'));
 })
 app.get('/contact-me', (req, res) => {
-    res.sendFile(path.join(__dirname,'./contact-me.html'));
+    res.sendFile(path.join(__dirname,'/contact-me.html'));
 })
 
 app.get('*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, './404.html'));
+    res.status(404).sendFile(path.join(__dirname, '/404.html'));
 })
 
 app.listen(port, () => {
